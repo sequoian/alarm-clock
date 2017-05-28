6a7d9af32217346b7b2916de3c91af1836f36c24
Zach Askins <zach303@msn.com>
2017-05-28 12:54:42 -0700

    working clock

--- src/App.js
+++ src/App.js
@@ -2,10 +2,37 @@ import React, { Component } from 'react';
 import './App.css';
 
 class Clock extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      datetime: null,
+      intervalID: null
+    }
+    this.tick = this.tick.bind(this);
+  }
+
+  componentWillMount() {
+    const date = new Date();
+    const interval = setInterval(this.tick, 1000);
+    this.setState({
+      datetime: date,
+      intervalID: interval
+    });
+  }
+
+  tick() {
+    const time = this.state.datetime;
+    time.setSeconds(time.getSeconds() + 1);
+    this.setState({
+      datetime: time
+    });
+  }
+
   render() {
+    const time = this.state.datetime.toLocaleTimeString()
     return (
       <div className={this.props.className}>
-        2:45pm
+        {time}
       </div>
     );
   }
