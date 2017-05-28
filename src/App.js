5849d719313e61335dda92fddee375e7e9109f62
Zach Askins <zach303@msn.com>
2017-05-28 12:16:50 -0700

    basic clock structure

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React, { Component } from 'react';
 import './App.css';
 
+class Clock extends Component {
+  render() {
+    return (
+      <div className={this.props.className}>
+        2:45pm
+      </div>
+    );
+  }
+}
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        Clock
+        <Clock 
+          className="clock"
+        />
       </div>
     );
   }
