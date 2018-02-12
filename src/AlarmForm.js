e24cf585cf55e75070c0223cf2401ed107bbccb2
Zach Askins <zach303@msn.com>
2018-02-12 12:04:19 -0800

    form styles

--- src/AlarmForm.js
+++ src/AlarmForm.js
@@ -2,24 +2,28 @@ import React from 'react'
 
 const AlarmForm = ({time, message, onChange, onSubmit, error, onBlur}) => (
   <form>
-    <span>{error}</span>
-    <input
-      type="text"
-      name="timeInput"
-      placeholder="Time"
-      value={time}
-      onChange={onChange}
-      onBlur={() => onBlur()}
-      onFocus={e => e.target.select()}
-      autoFocus
-    />
-    <input
-      type="text"
-      name="messageInput"
-      placeholder="Message (optional)"
-      value={message}
-      onChange={onChange}
-    />
+    <div id="inputs">
+      <span id="time-input">
+        <span className="error">{error}</span>
+        <input
+          type="text"
+          name="timeInput"
+          placeholder="Time"
+          value={time}
+          onChange={onChange}
+          onBlur={() => onBlur()}
+          onFocus={e => e.target.select()}
+          autoFocus
+        />
+      </span>
+      <input
+        type="text"
+        name="messageInput"
+        placeholder="Message (optional)"
+        value={message}
+        onChange={onChange}
+      />
+    </div>
     <button
       type="button"
       id="primary-btn"
